bfc265a03fa0115309a917431218904badd4394e
JHKim <atlas5420@naver.com>
2021-12-27 22:12:57 +0900

    초기 생성

---
+++ src/main/java/com/ISH/GoNZ/GoNzApplication.java
@@ -0,0 +1,13 @@
+package com.ISH.GoNZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoNzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoNzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ISH/GoNZ/GoNzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISH.GoNZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoNzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
